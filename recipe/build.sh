7704a8bc434856579662a5c1292803acc2e5660b
Isuru Fernando <isuruf@gmail.com>
2019-03-28 15:35:05 -0500

    Use llvm

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,15 +6,19 @@ cd build
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_BENCHMARKS=no \
-    -DINTEGER_CLASS=gmp \
+    -DINTEGER_CLASS=flint \
     -DWITH_SYMENGINE_THREAD_SAFE=yes \
     -DWITH_MPC=yes \
+    -DWITH_FLINT=yes \
+    -DWITH_LLVM=yes \
     -DBUILD_FOR_DISTRIBUTION=yes \
     -DBUILD_SHARED_LIBS=yes \
     ..
 
-cmake --build .
+cmake --build . -- -j${CPU_COUNT}
 cmake --build . --target install
 
 ctest
